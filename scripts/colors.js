3d972b270246c62766e2fec823ec2936876318b3
Balamurali M <balamurali05@outlook.com>
2019-05-11 21:52:47 +0530

    still stuff left

--- scripts/colors.js
+++ scripts/colors.js
@@ -1,14 +1,17 @@
+function getRandomIntInclusive(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		//The maximum is inclusive and the minimum is inclusive
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 //to give random color gradient background
 let color1 = '#'+(0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
 let color2 = '#'+(0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
-// TODO: try random direction too <08-05-19, Balamurali M> //
-// 0 to 360deg
-document.querySelector('body').style.backgroundImage = 
-	"linear-gradient(to bottom right, " + color1 + "," + color2 + ")";
+let color3 = '#'+(0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1,6);
+direction = getRandomIntInclusive(0, 360);
+document.querySelector('body').style.backgroundImage =
+		"repeating-linear-gradient(" + direction + "deg , " + color1 + ", " + color2 + ", " + color3 + ")";
 //color to links
 document.querySelectorAll('article a').forEach(function(a) {
-	a.style.color = color1;
+		a.style.color = color1;
 });
-// document.querySelectorAll('h1, h2').forEach(function(heading) {
-	// heading.style.borderBottomColor = color2;
-// });
